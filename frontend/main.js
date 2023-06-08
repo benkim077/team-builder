5803b42552e40177ad785d7160bd1cc4cd22c9b2
benkim077 <benkim0077@gmail.com>
2023-06-08 22:49:44 +0900

    FE: Function implementation complete

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,7 @@
 import { renderTable } from './src/util/renderTable.js';
-import { attachSubmitEventHandlerTo } from './src/util/attachEventTo.js';
+import { attachEventHandlerTo } from './src/util/attachEventHandlerTo.js';
 import { getUserRankTierBy } from './src/api/index.js';
+import { isEmptyObject } from './src/util/isEmptyObject.js';
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
@@ -13,12 +14,61 @@ document.querySelector('#app').innerHTML = `
 const submitEventHandler = async (event) => {
   event.preventDefault();
 
-  // TODO: 리팩토링
   const inputUsername = event.target.children[0].value;
 
   const res = await getUserRankTierBy(inputUsername);
-  console.log(res);
+
+  const string = getString(res);
+
+  function getString(res) {
+    if (isEmptyObject(res)) {
+      return null;
+    } else {
+      return Object.entries(res).reduce((acc, [key, value]) => {
+        return acc + `${key}: ${value} `;
+      }, '');
+    }
+  }
+
+  console.log(string);
+
+  renderSearchResult(event.target);
+
+  function renderSearchResult(targetNode) {
+    const elementId = targetNode.children[0].id;
+
+    const toBeRenderedHTML = `
+      <div id="search_result">
+        <a href="https://www.op.gg/summoner/userName=${inputUsername}" target="_blank">${inputUsername}</a>
+        <br />
+        <label for=${elementId}>${string}</label>
+      </div>
+    `;
+
+    const isAlreadyRendered = targetNode.children.length > 1;
+
+    if (isAlreadyRendered) {
+      targetNode.removeChild(targetNode.children[1]);
+    }
+
+    targetNode.innerHTML += toBeRenderedHTML;
+  }
+
+  event.target.children[0].value = inputUsername;
 };
 
 // 이벤트 상속하는 식으로 변경하자.
-attachSubmitEventHandlerTo(document.querySelectorAll('form'), submitEventHandler);
+attachEventHandlerTo('submit', document.querySelectorAll('form'), submitEventHandler);
+
+const clickEventHandler = (event) => {
+  const trElement = event.target.parentElement.parentElement;
+  const temp = trElement.children[1].innerHTML;
+
+  trElement.children[1].innerHTML = trElement.children[3].innerHTML;
+  trElement.children[3].innerHTML = temp;
+
+  // TODO: 이렇게 갔다 쓰면 안되는데... 수정하자.
+  attachEventHandlerTo('submit', document.querySelectorAll('form'), submitEventHandler);
+};
+
+attachEventHandlerTo('click', document.querySelectorAll('button'), clickEventHandler);
